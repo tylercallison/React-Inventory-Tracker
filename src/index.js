1c95f3ea2fa9e279ea65499d753df26428990834
tylercallison <tcsoccer2000@gmail.com>
2021-10-03 16:04:39 -0700

    added back web vitals

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import './styles/index.css';
 //import { FirebaseProvider } from "hooks/firestoreContext";
+import reportWebVitals from './reportWebVitals';
 
 import App from "./screens/App.js";
 import Test2 from "./screens/Test2.js";
@@ -22,3 +23,8 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
