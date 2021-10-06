fc1ae54dd7d378131497ae7800cb7b4c0a6e6c9d
tylercallison <tcsoccer2000@gmail.com>
2021-10-06 15:51:35 -0700

    added firebase and cache support

---
+++ src/config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFCxCoi1k7kRJyu9oJPARtZ7NrhUXkND4",
+  authDomain: "ice-track-d3065.firebaseapp.com",
+  projectId: "ice-track-d3065",
+  storageBucket: "ice-track-d3065.appspot.com",
+  messagingSenderId: "911130390469",
+  appId: "1:911130390469:web:6af7ce2acc40d90022a5ac"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
